2ebbf42ca761a1609b2c3f7d60a6336de05910df
Marco Bruno <marco.bruno@meliuz.com.br>
2022-03-08 14:14:28 +0100

    :sparkles: Create the splash screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Logo from "./components/Logo";
+import "./globalStyles/settings/colors.css";
+import "./globalStyles/generic/reset.css";
+import "./globalStyles/elements/base.css";
+import "./app.css";
 
 function App() {
-  return <h1>ChatCollab!</h1>;
+  return (
+    <main className="app">
+      <Logo />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "../../assets/images/logo.svg";
+import "./logo.css";
+
+function Logo() {
+  return <img className="logo" src={logo} alt="Logo da CollabChat" />;
+}
+
+export default Logo;
